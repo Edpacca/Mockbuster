0a21d8026b2e3108c4faff7be054b5c5b5a05cb1
Edpacca <eddie@pace.me.uk>
2021-01-28 16:20:07 +0000

    Test customer ViewModel created

---
+++ Mockbuster/ViewModels/TestCustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Mockbuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mockbuster.ViewModels
+{
+    public class TestCustomerViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
