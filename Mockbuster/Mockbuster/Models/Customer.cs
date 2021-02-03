560a1ea280528c68641a16d916198db1b1fe380d
Edpacca <eddie@pace.me.uk>
2021-02-03 10:58:07 +0000

    make Name non-nullable and set char limit

---
+++ Mockbuster/Mockbuster/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mockbuster.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        // e.g. Makes Name not nullable, must have a value.
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+    }
+}
