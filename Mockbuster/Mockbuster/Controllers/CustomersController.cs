4c3f07262278ae83c797805fc123413bcdde93c7
Edpacca <eddie@pace.me.uk>
2021-02-03 10:57:38 +0000

    Updated customer controller to get customers from database

--- Mockbuster/Mockbuster/Controllers/CustomersController.cs
+++ Mockbuster/Mockbuster/Controllers/CustomersController.cs
@@ -1,6 +1,6 @@
 ﻿using Mockbuster.Models;
 using Mockbuster.ViewModels;
-using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,10 +10,22 @@ namespace Mockbuster.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             var viewModel = new IndexCustomerViewModel
             {
@@ -21,24 +33,26 @@ namespace Mockbuster.Controllers
             };
 
             return View(viewModel);
-
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
 
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>()
-            {
-                new Customer() {Name = "Volo Geddarm", Id = 1 },
-                new Customer() {Name = "Vajra Safar", Id = 2 },
-                new Customer() {Name = "Lindir the Fair", Id = 3 }
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>()
+        //    {
+        //        new Customer() {Name = "Volo Geddarm", Id = 1 },
+        //        new Customer() {Name = "Vajra Safar", Id = 2 },
+        //        new Customer() {Name = "Lindir the Fair", Id = 3 }
+        //    };
+        //}
     }
 }
