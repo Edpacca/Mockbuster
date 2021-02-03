888c588dd5573fe00a183316e015f16d618e0df2
Edpacca <eddie@pace.me.uk>
2021-02-03 16:43:43 +0000

    Movies added to Identity model

--- Mockbuster/Mockbuster/Models/Movie.cs
+++ Mockbuster/Mockbuster/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,19 @@ namespace Mockbuster.Models
 {
     public class Movie
     {
+        [Requried]
         public int Id { get; set; }
+        [Requried]
+        [StringLength(255)]
         public string Name { get; set; }
+        [Requried]
+        [StringLength(124)]
+        public string Genre { get; set; }
+        [Requried]
+        public DateTime ReleaseDate { get; set; }
+        [Requried]
+        public DateTime DateAdded { get; set; }
+        [Requried]
+        public byte NumberInStock { get; set; }
     }
 }
