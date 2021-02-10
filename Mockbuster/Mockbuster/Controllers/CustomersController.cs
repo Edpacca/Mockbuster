be5677bb3959c71fc4754612333e5187ac1aa79c
Edpacca <eddie@pace.me.uk>
2021-02-10 13:48:44 +0000

    new ActionResults for Editing and Saving customer data

--- Mockbuster/Mockbuster/Controllers/CustomersController.cs
+++ Mockbuster/Mockbuster/Controllers/CustomersController.cs
@@ -22,6 +22,48 @@ namespace Mockbuster.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        // If actions modify data they should NEVER be accessable by HttpGet
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                // Not secure, and whitlisting arguments are magic strings
+                //TryUpdateModel(customerInDb, "" ne string[] { "Name", "Email" });
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+                // Or extension
+                // Mapper.Map(customer, customerInDb); 
+
+                // Alternatively make a new class which only has the updateable properties to restrict which properies can be updated
+            }
+
+            
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
@@ -33,6 +75,22 @@ namespace Mockbuster.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
         public ActionResult Details(int id)
