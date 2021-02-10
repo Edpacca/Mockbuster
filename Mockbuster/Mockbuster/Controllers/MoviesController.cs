c0736c1cf5331f860ba60cc7da7946016ec3d25b
Edpacca <eddie@pace.me.uk>
2021-02-10 15:24:01 +0000

    added ActionResults for movie creation form to MovieController

--- Mockbuster/Mockbuster/Controllers/MoviesController.cs
+++ Mockbuster/Mockbuster/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using Mockbuster.ViewModels;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,11 +24,6 @@ namespace Mockbuster.Controllers
             _context.Dispose();
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id =" + id);
-        }
-
         // Get: Movies
         public ActionResult Index()
         {
@@ -39,6 +35,54 @@ namespace Mockbuster.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         public ActionResult Details(int id)
