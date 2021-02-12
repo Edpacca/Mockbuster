c178725452b7f24046abfb4fbb9d60c59d1badf9
Edpacca <eddie@pace.me.uk>
2021-02-12 19:28:55 +0000

    changed movie property validation decorators

--- Mockbuster/Mockbuster/Models/Movie.cs
+++ Mockbuster/Mockbuster/Models/Movie.cs
@@ -21,13 +21,15 @@ namespace Mockbuster.Models
         [Display (Name = "Genre")]
         public byte GenreId { get; set; }
 
-        [Display (Name = "Release Date")]
-        public DateTime? ReleaseDate { get; set; }
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
 
         public DateTime? DateAdded { get; set; }
 
         [Required]
         [Display(Name = "Number In Stock")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
