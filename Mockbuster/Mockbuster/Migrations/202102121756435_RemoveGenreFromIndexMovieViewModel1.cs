1ae65d735df47d30acfc8b1469729aef3e074a06
Edpacca <eddie@pace.me.uk>
2021-02-12 17:59:00 +0000

    Migration history, fixing issue with Genre type in Movies

---
+++ Mockbuster/Mockbuster/Migrations/202102121756435_RemoveGenreFromIndexMovieViewModel1.cs
@@ -0,0 +1,16 @@
+namespace Mockbuster.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveGenreFromIndexMovieViewModel1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
