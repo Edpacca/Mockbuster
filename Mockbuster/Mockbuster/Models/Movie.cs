b5c1bb926ba2882f157bfcef616a9b9262b5af9b
Edpacca <eddie@pace.me.uk>
2021-02-12 18:02:09 +0000

    changed Genre from String to Genre class

--- Mockbuster/Mockbuster/Models/Movie.cs
+++ Mockbuster/Mockbuster/Models/Movie.cs
@@ -15,18 +15,16 @@ namespace Mockbuster.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        public string Genre { get; set; }
+        public Genre Genre { get; set; }
 
         [Required]
         [Display (Name = "Genre")]
         public byte GenreId { get; set; }
 
-        [Required]
         [Display (Name = "Release Date")]
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
 
-        [Required]
-        public DateTime DateAdded { get; set; }
+        public DateTime? DateAdded { get; set; }
 
         [Required]
         [Display(Name = "Number In Stock")]
