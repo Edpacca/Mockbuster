f1932f9a21358f167f3c4edfb2cd1a05d4ea0802
Edpacca <eddie@pace.me.uk>
2021-02-15 15:49:16 +0000

    created customer Dto

---
+++ Mockbuster/Mockbuster/Dtos/CustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using Mockbuster.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mockbuster.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        [Required(ErrorMessage = "Please select membership type")]
+
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
