5e75a429d9ff4b7954a3553842806cfa0e40483a
Edpacca <eddie@pace.me.uk>
2021-02-16 12:56:44 +0000

    set up Dto and domain mapping

--- Mockbuster/Mockbuster/App_Start/MappingProfile.cs
+++ Mockbuster/Mockbuster/App_Start/MappingProfile.cs
@@ -12,8 +12,15 @@ namespace Mockbuster.App_Start
     {
         public MappingProfile()
         {
+            // Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
