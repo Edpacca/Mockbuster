34bf3b0cdb007668d2fbe77c28e659f32fb1677d
Edpacca <eddie@pace.me.uk>
2021-03-02 20:26:23 +0000

    added typeahead js css bundle to project

--- Mockbuster/Mockbuster/App_Start/BundleConfig.cs
+++ Mockbuster/Mockbuster/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace Mockbuster
                       "~/Scripts/bootbox.js",
                       "~/Scripts/respond.js",
                       "~/Scripts/datatables/jquery.datatables.js",
-                      "~/Scripts/datatables/datatables.bootstrap.js"
+                      "~/Scripts/datatables/datatables.bootstrap.js",
+                      "~/Scripts/typeahead.bundle.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -28,7 +29,8 @@ namespace Mockbuster
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
-                      "~/Content/Site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/typeahead.css"));
         }
     }
 }
